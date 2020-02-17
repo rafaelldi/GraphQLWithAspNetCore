6084cdf5f794efa1cdbfd5b157496099b8ae02da
rafaelldi <rival.abdrakhmanov@outlook.com>
2020-02-17 21:27:02 +0300

    Add server project

---
+++ Server/Article.cs
@@ -0,0 +1,9 @@
+namespace Server
+{
+    public class Article
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Content { get; set; }
+    }
+}
