2fd346f94096707ac2c45fed9e09085b2ee33d08
rafaelldi <rival.abdrakhmanov@outlook.com>
2020-02-22 20:02:45 +0300

    Add filter

--- Server/GraphQL/BlogQuery.cs
+++ Server/GraphQL/BlogQuery.cs
@@ -1,3 +1,5 @@
+using System.Linq;
+using GraphQL;
 using GraphQL.Types;
 
 namespace Server.GraphQL
@@ -7,6 +9,17 @@ namespace Server.GraphQL
         public BlogQuery(ArticleService articleService)
         {
             Field<ListGraphType<ArticleType>>("list", resolve: context => articleService.Articles);
+            Field<ListGraphType<ArticleType>>(
+                "filtered-list", 
+                arguments: new QueryArguments
+                {
+                    new  QueryArgument<StringGraphType> { Name = "title"}    
+                },
+                resolve: context =>
+                {
+                    var titleFilter = context.GetArgument<string>("title");
+                    return articleService.Articles.Where(a => a.Title == titleFilter);
+                });
         }
     }
 }
